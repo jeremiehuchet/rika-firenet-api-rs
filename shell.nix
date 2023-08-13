54209bac25ba388fb293e8e9658946d1c8ec1158
Jeremie Huchet <jeremiehuchet@users.noreply.github.com>
2023-08-13 14:08:05 +0200

    ⬆️ Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 let
-  # release 22.11, 2023-05-19
-  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/03f2eb03bf67cda480680f82782889afa8d85b18.tar.gz")) {};
+  # release 23.05, 2023-08-13
+  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/90497216e09e9fe341fc3f2544398000cad33d20.tar.gz")) {};
   openapi-generator-cli-6_4 = pkgs.openapi-generator-cli.overrideAttrs (finalAttrs: previousAttrs: {
     version = "6.4.0";
     src = pkgs.fetchurl {
@@ -13,9 +13,9 @@ in pkgs.mkShell {
     openapi-generator-cli-6_4
     openssl
     pkg-config
-    rust_1_66.packages.stable.cargo
-    rust_1_66.packages.stable.rustc
-    rust_1_66.packages.stable.rustfmt
+    rust.packages.stable.cargo
+    rust.packages.stable.rustc
+    rust.packages.stable.rustfmt
     vscode
   ];
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
