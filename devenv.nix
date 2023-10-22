2293f443d6e2097afaa48b405220c0d51b8962cb
Jeremie Huchet <jeremiehuchet@users.noreply.github.com>
2023-10-22 07:12:49 +0200

    🧑‍💻 Setup devenv

---
+++ devenv.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+
+{
+  packages = [
+    pkgs.openapi-generator-cli
+    pkgs.openssl
+    pkgs.pkg-config
+    pkgs.vscode
+  ];
+
+  languages.rust = {
+    enable = true;
+    channel = "stable";
+  };
+
+  pre-commit.hooks.rustfmt.enable = true;
+
+  scripts.openapi-generate.exec = ''
+    openapi-generator-cli generate \
+        -g rust \
+        -o $DEVENV_ROOT/rika-firenet-openapi \
+        -i $DEVENV_ROOT/rika-firenet-openapi/rika-firenet-openapi.yml \
+        -c $DEVENV_ROOT/rika-firenet-openapi/openapi-config.yml
+  '';
+}
